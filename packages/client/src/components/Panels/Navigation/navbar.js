332682af7ac912f595d8bad0edaa0044571accaa
Justin Tracy <tracyjn@itscs-mini-206.ocas.uc.edu>
2022-10-31 16:46:44 -0400

    link to property form

--- packages/client/src/components/Panels/Navigation/navbar.js
+++ packages/client/src/components/Panels/Navigation/navbar.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
 export const Navigation = () => {
   return (
     <Navbar bg="dark" variant="dark">
       <Navbar.Brand href="/">Uni-Home</Navbar.Brand>
       <Nav className="me-auto">
-        <Nav.Link href="/">Properties</Nav.Link>
+        <NavDropdown title="Properties">
+          <NavDropdown.Item href="/">List</NavDropdown.Item>
+          <NavDropdown.Item href="/property/form">New</NavDropdown.Item>
+        </NavDropdown>
         <Nav.Link href="/chat">Roommate Chats</Nav.Link>
       </Nav>
     </Navbar>
-  )
+  );
 };
