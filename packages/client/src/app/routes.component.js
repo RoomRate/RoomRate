2d06e85c025203a26066f94987c3e079119ee7ff
hegartim <hegartim@mail.uc.edu>
2022-10-31 12:45:08 -0400

    Version 2

--- packages/client/src/app/routes.component.js
+++ packages/client/src/app/routes.component.js
@@ -3,9 +3,12 @@ import { Routes, Route } from 'react-router-dom';
 
 import { PropertyDetails } from '../components/Properties/propertyDetail';
 import { PropertyList } from '../components/Properties/propertyList';
+import { PropertyForm } from '../components/Properties/propertyForm'
+
 
 export const RoutesComponent = () =>
   <Routes>
     <Route path="/" element={<PropertyList />} />
     <Route path="/property/:id/detail" element={<PropertyDetails />} />
+    <Route path="/property/form" element={<PropertyForm />} />
   </Routes>;
