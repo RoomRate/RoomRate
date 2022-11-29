ad0c5be8fc4896c55c5ae28240c9633b8e597563
TonyTheMaccaroni <57689300+TonyTheMaccaroni@users.noreply.github.com>
2022-11-29 14:14:58 -0500

    heroku config

--- packages/server/src/utils/S3/index.js
+++ packages/server/src/utils/S3/index.js
@@ -1,15 +1,15 @@
 const S3 = require(`aws-sdk/clients/s3`);
 const config = require(`config`);
-const region = config.get(`s3.region`);
-const accessKeyId = config.get(`s3.aws-access-key`);
-const secretAccessKey = config.get(`s3.aws-secret-key`);
+const region = config.get(`s3.region`) || process.env.S3_REGION;
+const accessKeyId = config.get(`s3.aws-access-key`) || process.env.S3_AWS_ACCESS_KEY;
+const secretAccessKey = config.get(`s3.aws-secret-key`) || process.env.S3_AWS_SECRET_KEY;
 
 const s3 = new S3({ region, accessKeyId, secretAccessKey });
 
 // uploads file to s3
 exports.s3Upload = async ({ file, imageKey, mimetype }) => {
   const uploadImage = await s3.upload({
-    Bucket: config.get(`s3.bucket_name`),
+    Bucket: config.get(`s3.bucket_name`) || process.env.S3_BUCKET_NAME,
     Key: imageKey,
     Body: file,
     ContentType: mimetype,
@@ -22,7 +22,7 @@ exports.s3Upload = async ({ file, imageKey, mimetype }) => {
 
 exports.s3download = async (imgKey) => {
   const image = await s3.getObject({
-    Bucket: config.get(`s3.bucket_name`),
+    Bucket: config.get(`s3.bucket_name`) || process.env.S3_BUCKET_NAME,
     Key: imgKey,
   }).promise();
 
