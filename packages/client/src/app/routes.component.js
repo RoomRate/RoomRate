2aa95cecec9a38a06155b7df8f7fd66700a08476
hegartim <hegartim@mail.uc.edu>
2023-02-06 16:36:56 -0500

    Validation and forgot password page

--- packages/client/src/app/routes.component.js
+++ packages/client/src/app/routes.component.js
@@ -8,6 +8,7 @@ import { PropertyForm } from '../components/Properties/propertyForm';
 import { UserForm } from '../components/Users/userForm';
 import { LogIn } from 'components/Login/logIn';
 import { SignUp } from 'components/Login/signUp';
+import { ForgotPassword } from 'components/Login/forgotPass';
 
 export const RoutesComponent = () =>
   <Routes>
@@ -17,5 +18,6 @@ export const RoutesComponent = () =>
     <Route path="/property/form" element={<PropertyForm />} />
     <Route path="/user/form" element={<UserForm />} />
     <Route path="/login" element={<LogIn />} />
-    <Route path="/signup" element={<SignUp />} />
+    <Route path="/login/signup" element={<SignUp />} />
+    <Route path="/login/password" element={<ForgotPassword />} />
   </Routes>;
