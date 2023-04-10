32ba0e728fea39069f9ff9733da6ea4bf7233b46
TonyTheMaccaroni <57689300+TonyTheMaccaroni@users.noreply.github.com>
2023-04-10 19:40:39 -0400

    would this work

--- packages/server/src/utils/Geocoder/index.js
+++ packages/server/src/utils/Geocoder/index.js
@@ -3,8 +3,8 @@ const config = require(`config`);
 const geolib = require(`geolib`);
 
 const options = {
-  provider: config.get(`geocoder.provider`),
-  apiKey: config.get(`geocoder.apiKey`),
+  provider: config.get(`geocoder.provider`) || process.env.REACT_APP_GEOCODER_PROVIDER,
+  apiKey: config.get(`geocoder.apiKey`) || process.env.REACT_APP_GOOGLE_API_KEY,
 };
 
 const geocoder = NodeGeocoder(options);
@@ -16,7 +16,7 @@ exports.attachCoordinates = async ({ property }) => {
 
   const distance = await geolib.getDistance(
     { latitude, longitude },
-    config.get(`campus_coordinates`),
+    config.get(`campus_coordinates`) || { latitude: 39.131402309297805, longitude: -84.51615684482724 },
   );
   const distanceInMiles = (distance * 0.000621371).toFixed(2);
 
